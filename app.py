40d4632a94b11ebbde9db09506bb074e73bde6c4
Gioxx <giovanni@solone.it>
2025-05-13 14:21:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ def get_ip():
     """
     ip = request.headers.get('X-Forwarded-For', request.remote_addr)
     return ip + "\n"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
